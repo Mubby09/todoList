c72617da90f20305080a91863cf22b7b24918f4c
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2019-10-13 14:28:55 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+<div>
+  <p>hello world</p>
+  <input type= 'text' />
+
+
+</div>
+  )
+
 }
 
 export default App;
